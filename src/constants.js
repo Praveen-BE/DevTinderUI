6d6d7c5b656a7e3120ed6473550aca46bdc70cdc
Praveen-BE <praveenrajendiran3939@gmail.com>
2025-04-29 06:52:50 +0530

    base url update second time for ec2

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:7777";
+// export const BASE_URL = "http://localhost:7777";
 // export const BASE_URL = "http://13.232.150.48:7777";
-// export const BASE_URL = "http://13.232.150.48/api";
+export const BASE_URL = "http://13.232.150.48/api";
 
 export const defaultProfile =
   "https://th.bing.com/th?q=Default+Profile+Avatar+PNG&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247";
